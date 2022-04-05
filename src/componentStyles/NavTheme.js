2799c7b4abcda057fe63a07b30b546f6453fca62
SFPmedia <s-pauck@hotmail.com>
2022-04-05 10:14:48 +0200

    Removed redundant "color" change

--- src/componentStyles/NavTheme.js
+++ src/componentStyles/NavTheme.js
@@ -1,12 +1,11 @@
 import { createTheme } from "@mui/material";
-import { blue, lightBlue } from "@mui/material/colors";
+import { blue } from "@mui/material/colors";
 
 const theme = createTheme({
   components: {
     MuiLink: {
       styleOverrides: {
         root: {
-          color: lightBlue[50],
           marginLeft: "1em",
           backgroundColor: blue[500],
           borderRadius: "10px",
